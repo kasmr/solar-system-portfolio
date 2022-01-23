f5fd7b8759756747cad0270a1c4d4c282a51bf81
kasmr <alikkras@gmail.com>
2022-01-23 15:21:03 +0300

    Link

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -20,7 +20,7 @@ const Contacts = () => {
             <a href="https://www.linkedin.com/in/alik-krasnorutskiy-9348ba197/">
                 <SiLinkedin size="4rem"/>
             </a>
-            <a href="https://drive.google.com/file/d/1w0YSlcwksKK5Ru6GypQ0-ZzPK2zpzDRS/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/1OIpkp9v21woAhmHNadacfvHOt_R7_WHa/view?usp=sharing">
                 <FaFilePdf size="4rem"/>
             </a>
         </div>
